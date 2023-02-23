0ad948851dc15864d37baf1b0f574042580c143d
Yaroslav Volvach <volvachya@gmail.com>
2023-02-23 17:57:13 -0500

    Update Product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -11,9 +11,9 @@ Product.init(
   {
     id: {
       type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
       allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     product_name: {
       type: DataTypes.STRING,
@@ -23,25 +23,24 @@ Product.init(
       type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
-        isDecimal: true,
-      },
+        isDecimal: true
+      }
+    },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 10,
-      validator: {
+      defaultValue: [10],
+      validate: {
         isNumeric: true
-      },
-        
+      }
+    },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
         key: 'id'
-        }
       }
     }
-  }
   },
   {
     sequelize,
