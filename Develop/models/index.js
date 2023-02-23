809bfbbf9e15136a59ca870b1162902c7b95775b
Yaroslav Volvach <volvachya@gmail.com>
2023-02-23 13:01:38 -0500

    Add index.js for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,13 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category)
 
-// Categories have many Products
+Category.hasMany(Product)
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag)
 
 module.exports = {
   Product,
